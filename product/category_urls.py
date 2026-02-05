0b1c3edeb36ea2e0c3961bb281d6548c179e65c8
JUBAYER AHMMED <jubayerahmmed105@gmail.com>
2026-02-05 21:09:53 +0600

    ccbv1 first commit

--- product/category_urls.py
+++ product/category_urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from product import views
 urlpatterns = [
-    path('', views.view_categories, name = 'category-list'),
-    path('<int:pk>/', views.view_specific_category, name = 'specific_category'),
+    # path('', views.view_categories, name = 'category-list'),
+    path('', views.ViewCategories.as_view(), name = 'category-list'),
+    # path('<int:pk>/', views.view_specific_category, name = 'specific_category'),
+    path('<int:pk>/', views.ViewSpecificCategory.as_view(), name = 'specific_category'),
 ]
